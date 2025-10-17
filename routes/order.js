24a31d81f65d10049ee1c0d46fbb9061a0cdc55c
KingKacrae99 <kacrae9@gmail.com>
2025-10-18 00:09:57 +0100

    backend project updated

--- routes/order.js
+++ routes/order.js
@@ -6,10 +6,10 @@ const { utils } = require('../utils');
 const privilege = require("../middleware/auth")
 
 
-router.get('/',privilege.isAuthenticated,privilege.isStaff, wrapper(orderController.getOrders))
-router.get('/:id',privilege.isAuthenticated, privilege.isStaff, wrapper(orderController.getOrderById))
-router.put('/update/:id',privilege.isAuthenticated, privilege.isStaff,validateUpdateOrder, utils.handleValidationErrors, wrapper(orderController.updateOrder))
-router.post('/add', privilege.isAuthenticated ,privilege.isStaff, validateCreateOrder, utils.handleValidationErrors, wrapper(orderController.createOrder))
-router.delete('/delete/:id', privilege.isAuthenticated, privilege.isAdmin, wrapper(orderController.removeOrderById))
+router.get('/',/*privilege.isAuthenticated,privilege.isStaff,*/ wrapper(orderController.getOrders))
+router.get('/:id',/*privilege.isAuthenticated, privilege.isStaff,*/ wrapper(orderController.getOrderById))
+router.put('/update/:id',/*privilege.isAuthenticated, privilege.isStaff,*/validateUpdateOrder, utils.handleValidationErrors, wrapper(orderController.updateOrder))
+router.post('/add', /*privilege.isAuthenticated ,privilege.isStaff,*/ validateCreateOrder, utils.handleValidationErrors, wrapper(orderController.createOrder))
+router.delete('/delete/:id',/* privilege.isAuthenticated, privilege.isAdmin,*/ wrapper(orderController.removeOrderById))
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ const { UserRules } = require("../middleware/userValidation")
 const { utils } = require("../utils/index")
 const privilege = require("../middleware/auth")
 
-router.get('/', privilege.isAuthenticated, privilege.isAdmin, wrapper(userController.getUsers));
-router.put('/:id',privilege.isAuthenticated ,privilege.isAdmin, UserRules, utils.handleValidationErrors, wrapper(userController.updateUsers))
-router.get('/:id', privilege.isAuthenticated, wrapper(userController.getUser))
-router.delete('/:id',privilege.isAuthenticated, privilege.isAdmin,wrapper(userController.removeUser))
+router.get('/', /*privilege.isAuthenticated, privilege.isAdmin,*/ wrapper(userController.getUsers));
+router.put('/:id',/*privilege.isAuthenticated ,privilege.isAdmin,*/ UserRules, utils.handleValidationErrors, wrapper(userController.updateUsers))
+router.get('/:id', /*privilege.isAuthenticated,*/ wrapper(userController.getUser))
+router.delete('/:id',/*privilege.isAuthenticated, privilege.isAdmin,*/wrapper(userController.removeUser))
 
 module.exports = router
