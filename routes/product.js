801e2e35af08461daeb2614ea7bf9395e2b724f7
KingKacrae99 <kacrae9@gmail.com>
2025-10-18 00:00:29 +0100

    backend project updated

--- routes/product.js
+++ routes/product.js
@@ -5,10 +5,10 @@ const validator = require("../middleware/productValidation");
 const { utils } = require('../utils');
 const privilege = require("../middleware/auth")
 
-router.get('/',privilege.isAuthenticated, wrapper(productController.getProducts))
-router.get('/:id', privilege.isAuthenticated, wrapper(productController.getproduct))
-router.post('/add',privilege.isAuthenticated, privilege.isStaff,validator.validateAddProduct, utils.handleValidationErrors, wrapper(productController.addProductId))
-router.put('/edit/:id', privilege.isAuthenticated, privilege.isStaff, validator.validateAddProduct, utils.handleValidationErrors, wrapper(productController.updateProductId))
-router.delete('/delete/:id',privilege.isAuthenticated,privilege.isManagerOrAdmin, wrapper(productController.removeProductId))
+router.get('/', wrapper(productController.getProducts))
+router.get('/:id',wrapper(productController.getproduct))
+router.post('/add',/*privilege.isAuthenticated, privilege.isStaff,*/validator.validateAddProduct, utils.handleValidationErrors, wrapper(productController.addProductId))
+router.put('/edit/:id', /*privilege.isAuthenticated, privilege.isStaff,*/ validator.validateAddProduct, utils.handleValidationErrors, wrapper(productController.updateProductId))
+router.delete('/delete/:id',/*privilege.isAuthenticated,privilege.isManagerOrAdmin,*/ wrapper(productController.removeProductId))
 
 module.exports = router;
--- routes/supplier.js
+++ routes/supplier.js
@@ -8,10 +8,10 @@ const {
     validationHandler,
 } = require('../middleware/supplierValidation');
 
-router.post('/add',privilege.isAuthenticated,privilege.isManagerOrAdmin, validateAddSupplier, validationHandler, wrapper(supplierController.addSupplier))
-router.put('/update/:id',privilege.isAuthenticated,privilege.isAdmin, validateUpdateSupplier, validationHandler, wrapper(supplierController.updateSupplier))
-router.get('/:id',privilege.isAuthenticated, privilege.isManagerOrAdmin, wrapper(supplierController.getSupplier))
-router.get('',privilege.isAuthenticated,privilege.isManagerOrAdmin, wrapper(supplierController.getAllSupplier))
-router.delete('/delete/:id',privilege.isAuthenticated,privilege.isManagerOrAdmin,wrapper(supplierController.deleteSupplier))
+router.post('/add',/*privilege.isAuthenticated,privilege.isManagerOrAdmin,*/ validateAddSupplier, validationHandler, wrapper(supplierController.addSupplier))
+router.put('/update/:id',/*privilege.isAuthenticated,privilege.isAdmin,*/ validateUpdateSupplier, validationHandler, wrapper(supplierController.updateSupplier))
+router.get('/:id',/*privilege.isAuthenticated, privilege.isManagerOrAdmin,*/ wrapper(supplierController.getSupplier))
+router.get('',/*privilege.isAuthenticated,privilege.isManagerOrAdmin,*/ wrapper(supplierController.getAllSupplier))
+router.delete('/delete/:id',/*privilege.isAuthenticated,privilege.isManagerOrAdmin,*/wrapper(supplierController.deleteSupplier))
 
 module.exports = router;
